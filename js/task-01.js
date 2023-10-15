df204ea407ca21c0f07c44c5acb2dbe64f267a03
Mgalicka <j.malgosia1@wp.pl>
2023-10-15 12:05:24 +0200

    First commit

---
+++ js/task-01.js
@@ -0,0 +1,23 @@
+
+  const categoriesList = document.querySelector("#categories");
+  const categories = categoriesList.querySelectorAll("li.item");
+
+
+  console.log("Number of categories: " + categories.length);
+
+
+  categories.forEach((category) => {
+    const categoryName = category.querySelector("h2").textContent;
+    const categoryItems = category.querySelectorAll("ul li"); 
+
+    
+    console.log("Category: " + categoryName);
+    console.log("Elements: " + categoryItems.length);
+  });
+
+
+
+
+
+
+
