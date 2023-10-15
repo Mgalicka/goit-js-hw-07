26d5b7528eaa12b41f8646e13734566648f3f019
Mgalicka <j.malgosia1@wp.pl>
2023-10-15 12:09:05 +0200

    Second commit

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ul = document.getElementById("ingredients"); // Wybierz element ul#ingredients
+
+ingredients.forEach((ingredient) => {
+  const li = document.createElement("li"); // Utwórz element <li>
+  li.textContent = ingredient; // Dodaj tekst z tablicy jako zawartość tekstową
+  li.classList.add("item"); // Dodaj klasę "item" do elementu <li>
+  ul.appendChild(li); // Dodaj element <li> do listy <ul>
+});
