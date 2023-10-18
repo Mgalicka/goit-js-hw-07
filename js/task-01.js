c9379fb78466f0f38cf29a48b755e0f69945047b
Mgalicka <j.malgosia1@wp.pl>
2023-10-18 16:58:47 +0200

    First commit

--- js/task-01.js
+++ js/task-01.js
@@ -3,8 +3,7 @@
   const categories = categoriesList.querySelectorAll("li.item");
 
 
-  console.log("Number of categories: " + categories.length);
-
+  console.log("Number of categories: ", categories.length);
 
   categories.forEach((category) => {
     const categoryName = category.querySelector("h2").textContent;
